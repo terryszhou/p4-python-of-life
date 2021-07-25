8a6ac794d1789b7c07675b8b844d2db52045ac9c
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-25 16:37:28 -0500

    Full game grid rendered.

--- game_of_life.py
+++ game_of_life.py
@@ -3,7 +3,7 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((757,757))
 pygame.display.set_caption("Terry's Game of Life")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("fonts/Pixeltype.ttf", 50)
@@ -11,11 +11,13 @@ game_active = True
 
 cell_width = 20
 cell_height = 20
-margin = 5
+margin = 1
 
 grid = []
-for column in range(10):
-    grid.append(0)
+for row in range(36):
+    grid.append([])
+    for column in range(36):
+        grid[row].append(0)
 
 cell_surf = pygame.Surface((cell_width, cell_height))
 cell_surf.fill("white")
@@ -26,8 +28,16 @@ while True:
             pygame.quit()
             exit()
 
-    for column in range(10):
-        pygame.draw.rect(screen, "white", [(margin + cell_width) * column + margin, margin + 0, cell_width, cell_height])
+    if game_active == True:
+        screen.fill("grey")
+        for row in range(36):
+            for column in range(36):
+                pygame.draw.rect(screen, "black", [
+                    (margin + cell_width) * column + margin,
+                    (margin + cell_height) * row + margin,
+                    cell_width,
+                    cell_height
+                ])
 
 
 
