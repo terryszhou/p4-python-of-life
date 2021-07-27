a8095a2db2a9dd36febb206b8585d01abd3bbc16
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-27 15:11:46 -0500

    Refactored pause screen variables into separate class.

--- game_of_life.py
+++ game_of_life.py
@@ -8,7 +8,7 @@ from sys import exit
 pygame.init()
 pygame.display.set_caption("Terry's Game of Life")
 clock = pygame.time.Clock()
-my_font = pygame.font.Font("fonts/Pixeltype.ttf", 35)
+my_font = pygame.font.Font("fonts/Pixeltype.ttf", 40)
 title_font = pygame.font.Font("fonts/Pixeltype.ttf", 75)
 pause = True
 screen_width = 757
@@ -90,47 +90,67 @@ class Audio:
         self.unclick = pygame.mixer.Sound("audio/unclick.wav")
         self.destroy = pygame.mixer.Sound("audio/destroy.wav")
 
-        self.run_sim.set_volume(0.03)
-        self.pause_sim.set_volume(0.05)
-        self.click.set_volume(0.05)
-        self.unclick.set_volume(0.05)
-        self.destroy.set_volume(0.05)
+        self.run_sim.set_volume(0.06)
+        self.pause_sim.set_volume(0.1)
+        self.click.set_volume(0.1)
+        self.unclick.set_volume(0.1)
+        self.destroy.set_volume(0.1)
 
         self.bg_music = pygame.mixer.music
         self.bg_music.load("audio/dont-forget-me.mp3")
-        self.bg_music.set_volume(0.05)
+        self.bg_music.set_volume(0.1)
         self.bg_music.play(-1)
+
+# # PAUSE SCREEN - - - - - - - - - - - - - - - - -
+class Pause_Screen:
+    def __init__(self):
+        # self.pause == True
+        self.pause_surf = pygame.Surface((screen_width,screen_height), pygame.SRCALPHA)
+        self.pause_surf.fill((0,0,0,180))
+
+        self.title = title_font.render("TERRY'S GAME OF LIFE", False, "white")
+        self.title_rect = self.title.get_rect(center = (380,100))
+
+        self.rule_1 = my_font.render("1. Any live cell with 2 or 3 live neighbors survives.", True, "white")
+        self.rule_2_pt_1 = my_font.render("2. Any dead cell with exact 3 live", False, "white")
+        self.rule_2_pt_2 = my_font.render("neighbors comes to life.", False, "white")
+        self.rule_3 = my_font.render("3. All other cells die.", False, "white")
+
+        self.rule_1_rect = self.rule_1.get_rect(center = (380,200))
+        self.rule_2_pt_1_rect = self.rule_2_pt_1.get_rect(center = (380,300))
+        self.rule_2_pt_2_rect = self.rule_2_pt_2.get_rect(center = (380,330))
+        self.rule_3_rect = self.rule_3.get_rect(center = (380,430))
+
+        self.instruction_1 = my_font.render("<CLICK> to bring cells to life", True, "white")
+        self.instruction_2 = my_font.render("<SPACEBAR> to pause/unpause", True, "white")
+        self.instruction_3 = my_font.render("<Q>, when paused, to clear board", True, "white")
+        self.instruction_4 = my_font.render("<M> to pause/unpause music", True, "white")
+
+        self.instruction_1_rect = self.instruction_1.get_rect(center = (380,600))
+        self.instruction_2_rect = self.instruction_2.get_rect(center = (380,630))
+        self.instruction_3_rect = self.instruction_3.get_rect(center = (380,660))
+        self.instruction_4_rect = self.instruction_4.get_rect(center = (380,690))
+
+    def draw_pause(self):
+        if pause == True:
+            screen.blit(self.pause_surf, (0,0))
+            screen.blit(self.title, self.title_rect)
+            screen.blit(self.rule_1, self.rule_1_rect)
+            screen.blit(self.rule_2_pt_1, self.rule_2_pt_1_rect)
+            screen.blit(self.rule_2_pt_2, self.rule_2_pt_2_rect)
+            screen.blit(self.rule_3, self.rule_3_rect)
+            screen.blit(self.instruction_1, self.instruction_1_rect)
+            screen.blit(self.instruction_2, self.instruction_2_rect)
+            screen.blit(self.instruction_3, self.instruction_3_rect)
+            screen.blit(self.instruction_4, self.instruction_4_rect)
+
+    def run(self):
+        self.draw_pause()
 
 # # INSTANTIATE CLASS VARIABLES - - - - - - - - - - - - - - - - -
 game_of_life = Game_Of_Life()
 audio = Audio()
-
-# # PAUSE SCREEN RULES & INSTRUCTIONS - - - - - - - - - - - - - - - - -
-pause_surf = pygame.Surface((screen_width,screen_height), pygame.SRCALPHA)
-pause_surf.fill((0,0,0,180))
-
-title = title_font.render("TERRY'S GAME OF LIFE", False, "white")
-title_rect = title.get_rect(center = (380,100))
-
-rule_1 = my_font.render("1. Any live cell with 2 or 3 live neighbors survives.", True, "white")
-rule_2_pt_1 = my_font.render("2. Any dead cell with exact 3 live", False, "white")
-rule_2_pt_2 = my_font.render("neighbors comes to life.", False, "white")
-rule_3 = my_font.render("3. All other cells die.", False, "white")
-
-rule_1_rect = rule_1.get_rect(center = (380,200))
-rule_2_pt_1_rect = rule_2_pt_1.get_rect(center = (380,300))
-rule_2_pt_2_rect = rule_2_pt_2.get_rect(center = (380,330))
-rule_3_rect = rule_3.get_rect(center = (380,430))
-
-instruction_1 = my_font.render("<CLICK> to bring cells to life", True, "white")
-instruction_2 = my_font.render("<SPACEBAR> to pause/unpause", True, "white")
-instruction_3 = my_font.render("<Q>, when paused, to clear board", True, "white")
-instruction_4 = my_font.render("<M> to pause/unpause music", True, "white")
-
-instruction_1_rect = instruction_1.get_rect(center = (380,600))
-instruction_2_rect = instruction_2.get_rect(center = (380,630))
-instruction_3_rect = instruction_3.get_rect(center = (380,660))
-instruction_4_rect = instruction_4.get_rect(center = (380,690))
+pause_screen = Pause_Screen()
 
 # # MAIN GAME LOOP - - - - - - - - - - - - - - - - -
 while True:
@@ -140,8 +160,8 @@ while True:
             pygame.quit()
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN: # <-- mark cells as living or dead
-            col = int(event.pos[0]/(game_of_life.cell_width + game_of_life.margin))
             row = int(event.pos[1]/(game_of_life.cell_height + game_of_life.margin))
+            col = int(event.pos[0]/(game_of_life.cell_width + game_of_life.margin))
             if game_of_life.grid[row][col] == 0:
                 audio.click.play()
                 game_of_life.grid[row][col] = 1
@@ -171,19 +191,7 @@ while True:
 
     # # RUN GAME - - - - - - - - - - - - - - - - - - -
     game_of_life.run()
-
-    # # RENDER PAUSE SCREEN - - - - - - - - - - - - - - - - - - -
-    if pause == True:
-        screen.blit(pause_surf, (0,0))
-        screen.blit(title, title_rect)
-        screen.blit(rule_1, rule_1_rect)
-        screen.blit(rule_2_pt_1, rule_2_pt_1_rect)
-        screen.blit(rule_2_pt_2, rule_2_pt_2_rect)
-        screen.blit(rule_3, rule_3_rect)
-        screen.blit(instruction_1, instruction_1_rect)
-        screen.blit(instruction_2, instruction_2_rect)
-        screen.blit(instruction_3, instruction_3_rect)
-        screen.blit(instruction_4, instruction_4_rect)
+    pause_screen.run()
 
     # # UPDATE CLOCK AND DISPLAY - - - - - - - - - - - - - - - - - - -
     pygame.display.update()
