d30ca2c049dd0d244d00a3685597de74a584b409
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-29 09:15:38 -0500

    Slightly refactored for cleaner code, lengths/widths made relative to variables.

--- game_of_life.py
+++ game_of_life.py
@@ -6,8 +6,8 @@ from sys import exit
 # # BASIC VARIABLES - - - - - - - - - - - - - - - - -
 py.init()
 py.display.set_caption("Terry's Game of Life")
-screen_width = 730
-screen_height = 730
+screen_width = 725
+screen_height = 725
 screen = py.display.set_mode((screen_width,screen_height))
 screen.fill("black")
 clock = py.time.Clock()
@@ -108,7 +108,7 @@ class Game_Of_Life:
                 if neighbors_count < 2 or neighbors_count > 3:
                     current_state = 0
             else:
-                if neighbors_count == 3 or neighbors_count == 6:
+                if neighbors_count in (3,6):
                     current_state = 1
             return current_state
 
@@ -138,19 +138,6 @@ class Game_Of_Life:
         self.grid[22][22] = 1
 
     def spawn_replicator(self):
-        # self.grid[16][17] = 1
-        # self.grid[16][18] = 1
-        # self.grid[16][19] = 1
-        # self.grid[17][16] = 1
-        # self.grid[17][19] = 1
-        # self.grid[18][15] = 1
-        # self.grid[18][19] = 1
-        # self.grid[19][15] = 1
-        # self.grid[19][18] = 1
-        # self.grid[20][15] = 1
-        # self.grid[20][16] = 1
-        # self.grid[20][17] = 1
-        
         self.grid[20][21] = 1
         self.grid[20][22] = 1
         self.grid[20][23] = 1
@@ -183,13 +170,10 @@ class Audio:
         self.switch_mode = py.mixer.Sound("audio/switch_mode.wav")
         self.spawn = py.mixer.Sound("audio/spawn.wav")
 
-        self.run_sim.set_volume(0.06)
-        self.pause_sim.set_volume(0.1)
-        self.click.set_volume(0.1)
-        self.unclick.set_volume(0.1)
-        self.destroy.set_volume(0.1)
-        self.switch_mode.set_volume(0.1)
-        self.spawn.set_volume(0.1)
+        self.volume_list = []
+        for x in [self.run_sim, self.pause_sim, self.click, self.unclick, self.destroy, self.switch_mode, self.spawn]:
+            def res(x): return x.set_volume(0.1)
+            self.volume_list.append(res(x))
 
         self.bg_music = py.mixer.music
         self.bg_music.load("audio/dont-forget-me.mp3")
@@ -209,32 +193,32 @@ class Pause_Screen:
         self.gol_rule_2_pt_2 = my_font.render("neighbors comes to life.", False, "white")
         self.gol_rule_3 = my_font.render("3. All other cells die.", False, "white")
 
-        self.gol_title_rect = self.gol_title.get_rect(center = (screen_width/2,100))
-        self.gol_rule_1_rect = self.gol_rule_1.get_rect(center = (screen_width/2,200))
-        self.gol_rule_2_pt_1_rect = self.gol_rule_2_pt_1.get_rect(center = (screen_width/2,300))
-        self.gol_rule_2_pt_2_rect = self.gol_rule_2_pt_2.get_rect(center = (screen_width/2,330))
-        self.gol_rule_3_rect = self.gol_rule_3.get_rect(center = (screen_width/2,430))
+        self.gol_title_rect = self.gol_title.get_rect(center = (screen_width/2, screen_height/7))
+        self.gol_rule_1_rect = self.gol_rule_1.get_rect(center = (screen_width/2, screen_height/(7/2)))
+        self.gol_rule_2_pt_1_rect = self.gol_rule_2_pt_1.get_rect(center = (screen_width/2, screen_height/(7/2.8)))
+        self.gol_rule_2_pt_2_rect = self.gol_rule_2_pt_2.get_rect(center = (screen_width/2, screen_height/(7/3.2)))
+        self.gol_rule_3_rect = self.gol_rule_3.get_rect(center = (screen_width/2, screen_height/(7/4)))
 
         # # LIFE WITHOUT DEATH DISPLAY - - - - - - - - - - - - - - -
         self.lwd_title = title_font.render("TERRY'S LIFE WITHOUT DEATH", False, "red")
         self.lwd_rule_1 = my_font.render("1. Cells do not die.", True, "white")
         
-        self.lwd_title_rect = self.lwd_title.get_rect(center = (screen_width/2,100))
-        self.lwd_rule_1_rect = self.lwd_rule_1.get_rect(center = (screen_width/2,200))
+        self.lwd_title_rect = self.lwd_title.get_rect(center = (screen_width/2, screen_height/7))
+        self.lwd_rule_1_rect = self.lwd_rule_1.get_rect(center = (screen_width/2, screen_height/(7/2)))
 
         # # MAZE DISPLAY - - - - - - - - - - - - - - -
         self.mz_title = title_font.render("TERRY'S MAZE", False, "purple")
         self.mz_rule_1 = my_font.render("1. Any living cell with 2, 3, or 4 living neighbors survives.", True, "white")
 
-        self.mz_title_rect = self.mz_title.get_rect(center = (screen_width/2,100))
-        self.mz_rule_1_rect = self.mz_rule_1.get_rect(center = (screen_width/2,200))
+        self.mz_title_rect = self.mz_title.get_rect(center = (screen_width/2, screen_height/7))
+        self.mz_rule_1_rect = self.mz_rule_1.get_rect(center = (screen_width/2, screen_height/(7/2)))
 
         # # HIGH LIFE DISPLAY
         self.hl_title = title_font.render("TERRY'S HIGH LIFE", False, "gold")
         self.hl_rule_2_pt_1 = my_font.render("2. Any dead cell with 3 or 6 living", False, "white")
 
-        self.hl_title_rect = self.hl_title.get_rect(center = (screen_width/2,100))
-        self.h1_rule_2_pt_1_rect = self.hl_rule_2_pt_1.get_rect(center = (screen_width/2,300))
+        self.hl_title_rect = self.hl_title.get_rect(center = (screen_width/2, screen_height/7))
+        self.h1_rule_2_pt_1_rect = self.hl_rule_2_pt_1.get_rect(center = (screen_width/2, screen_height/(7/2.8)))
 
         # # GAME INSTRUCTIONS - - - - - - - - - - - - - - -
         self.inst_1 = my_font.render("<CLICK> --- resurrect/kill cells", True, "white")
@@ -244,13 +228,12 @@ class Pause_Screen:
         self.inst_5 = my_font.render("<ARROW KEYS> --- change rulesets", True, "white")
         self.inst_6 = my_font.render("Try <G>, <B>, <H>, <R>, and <Q>!", True, "white")
 
-        self.inst_1_rect = self.inst_1.get_rect(center = (screen_width/2,520))
-        self.inst_2_rect = self.inst_2.get_rect(center = (screen_width/2,550))
-        self.inst_3_rect = self.inst_3.get_rect(center = (screen_width/2,580))
-        self.inst_4_rect = self.inst_4.get_rect(center = (screen_width/2,610))
-        self.inst_5_rect = self.inst_5.get_rect(center = (screen_width/2,640))
-        self.inst_5_rect = self.inst_5.get_rect(center = (screen_width/2,670))
-        self.inst_6_rect = self.inst_6.get_rect(center = (screen_width/2,700))
+        self.inst_1_rect = self.inst_1.get_rect(center = (screen_width/2, screen_height/(7/5)))
+        self.inst_2_rect = self.inst_2.get_rect(center = (screen_width/2, screen_height/(7/5.3)))
+        self.inst_3_rect = self.inst_3.get_rect(center = (screen_width/2, screen_height/(7/5.6)))
+        self.inst_4_rect = self.inst_4.get_rect(center = (screen_width/2, screen_height/(7/5.9)))
+        self.inst_5_rect = self.inst_5.get_rect(center = (screen_width/2, screen_height/(7/6.2)))
+        self.inst_6_rect = self.inst_6.get_rect(center = (screen_width/2, screen_height/(7/6.5)))
 
     def draw_pause(self): # <-- draws variables above if game is paused.
         if pause == True:
@@ -307,14 +290,14 @@ def display_generation():
 # # MAIN GAME LOOP - - - - - - - - - - - - - - - - -
 while True:
     # # EVENT CONDITIONALS - - - - - - - - - - - - - - - - -
-    for event in py.event.get(): # <-- gets game events, loops through them
-        if event.type == py.QUIT: # <-- closes out window if event type is QUIT
+    for e in py.event.get(): # <-- gets game events, loops through them
+        if e.type == py.QUIT: # <-- closes out window if event type is QUIT
             py.quit()
             exit()
-        if event.type == py.MOUSEBUTTONDOWN: # <-- marks cells as living or dead
-            if event.button == 1:
-                row = int(event.pos[1]/(game_of_life.cell_height + game_of_life.margin))
-                col = int(event.pos[0]/(game_of_life.cell_width + game_of_life.margin))
+        if e.type == py.MOUSEBUTTONDOWN: # <-- marks cells as living or dead
+            if e.button == 1:
+                row = int(e.pos[1]/(game_of_life.cell_height + game_of_life.margin))
+                col = int(e.pos[0]/(game_of_life.cell_width + game_of_life.margin))
                 if game_of_life.grid[row][col] == 0:
                     audio.click.play()
                     game_of_life.grid[row][col] = 1
@@ -322,62 +305,46 @@ while True:
                     audio.unclick.play()
                     game_of_life.grid[row][col] = 0
                 print(f"Row: {row}, Column: {col}")
-        if event.type == py.KEYDOWN:
-            if event.key == py.K_m: # <-- pause/unpause music
+        if e.type == py.KEYDOWN:
+            if e.key == py.K_m: # <-- pause/unpause music
                 if audio.bg_music.get_busy() == 1:
                     audio.bg_music.pause()
                 else:
                     audio.bg_music.unpause()
-            if event.key == py.K_SPACE: # <-- pause/unpause game
+            if e.key == py.K_SPACE: # <-- pause/unpause game
                 audio.pause_sim.play()
                 if pause == False:
                     pause = True
                 else:
                     pause = False
-            if event.key == py.K_c: # <-- clear board if game is paused
+            if e.key == py.K_c: # <-- clear board if game is paused
                 audio.destroy.play()
                 generation = 0
                 for row in range(game_of_life.rows):
                     for column in range(game_of_life.cols):
                         game_of_life.grid[row][column] = 0
-            if event.key == py.K_UP or event.key == py.K_LEFT or event.key == py.K_RIGHT or event.key == py.K_DOWN:
+            if e.key in (py.K_UP, py.K_LEFT, py.K_RIGHT, py.K_DOWN):
                 audio.switch_mode.play()
                 generation = 0
-                if event.key == py.K_UP: # <-- switch to Game of Life rules
+                if e.key == py.K_UP: # <-- switch to Game of Life rules
+                    lwd_rules = mz_rules = hl_rules = False
                     gol_rules = True
-                    lwd_rules = False
-                    mz_rules = False
-                    hl_rules = False
-                if event.key == py.K_LEFT: # <-- switch to Life Without Death rules
-                    gol_rules = False
+                if e.key == py.K_LEFT: # <-- switch to Life Without Death rules
+                    gol_rules = mz_rules = hl_rules = False
                     lwd_rules = True
-                    mz_rules = False
-                    hl_rules = False
-                if event.key == py.K_RIGHT: # <-- switch to Maze rules
-                    gol_rules = False
-                    lwd_rules = False
+                if e.key == py.K_RIGHT: # <-- switch to Maze rules
+                    gol_rules = lwd_rules = hl_rules = False
                     mz_rules = True
-                    hl_rules = False
-                if event.key == py.K_DOWN: # <-- switch to High Life rules
-                    gol_rules = False
-                    lwd_rules = False
-                    mz_rules = False
+                if e.key == py.K_DOWN: # <-- switch to High Life rules
+                    gol_rules = lwd_rules = mz_rules = False
                     hl_rules = True
-            if event.key == py.K_g: # <-- spawns glider
+            if e.key in (py.K_g, py.K_b, py.K_h, py.K_r, py.K_q):
                 audio.spawn.play()
-                game_of_life.spawn_glider()
-            if event.key == py.K_b: # <-- spawns blinker
-                audio.spawn.play()
-                game_of_life.spawn_beacon()
-            if event.key == py.K_h: # <-- spawns hive
-                audio.spawn.play()
-                game_of_life.spawn_hive()
-            if event.key == py.K_r: # <-- spawns hive
-                audio.spawn.play()
-                game_of_life.spawn_replicator()
-            if event.key == py.K_q: # <-- spawns random grid
-                audio.spawn.play()
-                game_of_life.spawn_random()
+                if e.key == py.K_g: game_of_life.spawn_glider() # <-- spawns glider
+                if e.key == py.K_b: game_of_life.spawn_beacon() # <-- spawns blinker
+                if e.key == py.K_h: game_of_life.spawn_hive() # <-- spawns hive
+                if e.key == py.K_r: game_of_life.spawn_replicator() # <-- spawns hive
+                if e.key == py.K_q: game_of_life.spawn_random() # <-- spawns random grid
 
     # # RUN SIMULATION - - - - - - - - - - - - - - - - - - -
     game_of_life.run()
